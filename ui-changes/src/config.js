de5f558b38e5c839cb0209bbead783af75589875
Alisha Kodibagkar <46933681+alishakodibagkar@users.noreply.github.com>
2023-01-18 00:27:49 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Screening/master/protocol/Screening_schema',
+  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Baseline/master/protocol/Baseline_schema',
   startButton: {
     "en": 'Join'
   },
-  banner: 'Eligibility Screening for Mumble Melody Longitudinal Study',
-  assetsPublicPath: '/MumbleMelody_Longitudinal_Screening/',
+  banner: 'Baseline Testing Session for Mumble Melody Longitudinal Study',
+  assetsPublicPath: '/MumbleMelody_Longitudinal_Baseline/',
   contact: 'akodiba1@mit.edu',
   showHelp: true,
   checkMediaPermission: true
